a66e5bc376facb11158dca97034e2df1fe8fcafe
Dmitry <kulik.dmitriy.2013@yandex.ru>
2021-06-16 12:50:40 +0700

    Added model classes

---
+++ WorkersFeature/Models/Person.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace WorkersFeature.Models
+{
+    public class Person
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public string DisplayName { get; set; }
+        
+        public virtual List<Skill> Skills { get; set; }
+    }
+}
---
+++ WorkersFeature/Models/Skill.cs
@@ -0,0 +1,11 @@
+﻿namespace WorkersFeature.Models
+{
+    public class Skill
+    {
+        public string Name { get; set; }
+        public byte Level { get; set; }
+        public int? PersonId { get; set; }
+        
+        public virtual Person Person { get; set; }
+    }
+}
